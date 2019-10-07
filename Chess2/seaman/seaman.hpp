b708b0a4aa5f5cf61334740835f6f1bd62906a70
Przemek <pshemeko@users.noreply.github.com>
2019-10-07 14:48:19 +0200

    Zmienilem marynarza w pracy

--- Chess2/seaman/seaman.hpp
+++ Chess2/seaman/seaman.hpp
@@ -25,7 +25,9 @@ public:
         while(!stopThread) {
             //Sleep(1000);
             //std::this_thread::sleep_for(100ms);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
+            //std::cout<<"dupa koniec counter : " << counter;
     }
 private:
 
@@ -33,16 +35,16 @@ private:
         std::lock_guard<std::mutex> lk(mtx);
         std::cout << '|';
         //if (counter == 0) std::cout <<'*';
-        for(unsigned i = 0; i< counter; ++i)
+        for(unsigned i = 0; i < counter; ++i)
         {
             std::cout << '-';
         }
         std::cout << '*';        
-        for(unsigned i = counter; i< roadWidth; ++i)
+        for(unsigned i = counter; i < roadWidth - 1; ++i)
         {
             std::cout << '-';
         }
-        std::cout <<'|' << '/n';
+        std::cout <<'|' << '\n';
     }
 
     void changePosition(int delta) {
@@ -50,12 +52,15 @@ private:
         while(!stopThread)
         {
             {      
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::this_thread::sleep_for(std::chrono::milliseconds(100 + delta * 9));
                 //std::thread(&SeaMan::changePosition, this, delta);   
                 
                 if ((counter == 0 ) || ( counter == roadWidth -1)) { 
                     stopThread = true;
                     show();
+                    std::cout<<"counter : " << counter;
+                    break;
                 } else {
                 counter +=delta;
                 show();
@@ -63,6 +68,7 @@ private:
 
             }
         }
+        //show();
     }
 
     std::thread thrdLeft;
